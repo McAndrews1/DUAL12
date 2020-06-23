05d14e8304f8c9475c48292988abe6cfb601adde
McAndrews1 <lgmcandrews@gmail.com>
2020-06-23 04:56:18 +0200

    En esta version se ha actualizado la documentacion de la clase doc

--- doc.java
+++ doc.java
@@ -1,14 +1,23 @@
  
 /**
- * Write a description of class doc here.
+ * Devuelve el contenido del archivo tras insertar la linea line en la 
+ * linea numero x, el contenido de la linea x, pasa a ser la linea x+1 y asi sucesivamente.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Mykola Oliinyk) 
+ * @version (1.2)
  */
 public interface doc
 {
    
    // Devuelve el contenido del archivo tras insertar la linea line en la linea numero x, el contenido de la linea x, pasa a ser la linea x+1 y asi sucesivamente.
+   /**
+    * Devuelve el contenido del archivo tras insertar la linea line en la 
+    * linea numero x, el contenido de la linea x, pasa a ser la linea x+1 y asi sucesivamente.
+    * 
+    * @param    x   numero de linea en la que se va a insertar line
+    *           line    la cadena que contiene la iformacion que se insertará en la linea x
+    *           file    el archivo en el que se insertaraá line
+    */
    public abstract String escribirLinea(int x, String line, String file);
    
    
