508a2a3e82e1dc8d5212a0aa5268d0b122576381
McAndrews1 <lgmcandrews@gmail.com>
2020-06-23 05:06:52 +0200

    Se ha creado y se ha documentado un metodo nuevo en la clase doc llamado devolverPerimetro

--- doc.java
+++ doc.java
@@ -1,7 +1,7 @@
  
 /**
- * Devuelve el contenido del archivo tras insertar la linea line en la 
- * linea numero x, el contenido de la linea x, pasa a ser la linea x+1 y asi sucesivamente.
+ * Tiene dos metodos: para calcular el perimetro de una ciudad y otro para devolver contenido de un archivo 
+ * despues de haber insertado lineas en un numero de linea determinado
  * 
  * @author (Mykola Oliinyk) 
  * @version (1.2)
@@ -19,6 +19,15 @@ public interface doc
     *           file    el archivo en el que se insertaraá line
     */
    public abstract String escribirLinea(int x, String line, String file);
+   
+   /**
+    * Devuelve el perimetro de una ciudad en kilometros cuadrados
+    * 
+    * @param    ancho   lo que mide la ciudad a lo ancho
+    *           largo    lo que mide la ciudad a lo largo
+    *           ciudad    nombre de la ciudad que se vaya a calcular el perimetro
+    */
+   public abstract int devolverPerimetro(int ancho, int largo, String ciudad);
    
    
 }
